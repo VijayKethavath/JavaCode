99889bcd95bb8f46178529460f3c3736f17302d3
kethavathvijayrathod <kethavathvijay2210@gmail.com>
2026-04-09 19:02:43 +0530

    unique character

--- Code/src/code/uniquechar.java
+++ Code/src/code/uniquechar.java
@@ -12,25 +12,25 @@ public class uniquechar {
 				if(str.charAt(i)== str.charAt(j)) {
 					count++;
 					arr[j]=true;
-					
+
 				}
 			}
 			if(count==1) {
-			System.out.println(str.charAt(i)+"->"+count);
+				System.out.println(str.charAt(i)+"->"+count);
 			}
 			else {
 				System.out.println("duplicated values :"+str.charAt(i)+"->"+count);
 			}
 		}
 		return 0;
-		
-		
+
+
 	}
 
 	public static void main(String[] args) {
 		String str="leetcode";
 		unique(str);
-		
+
 
 	}
 
